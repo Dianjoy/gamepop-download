79fd017a1b2874f2961e8e3024118c09dbcff3d0
baibai <xiangbai89314@gmail.com>
2014-07-25 18:55:49 +0800

    手机配置

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@
  * Created by 路佳 on 2014/7/24.
  */
 var reg = /micromessenger/i
-  , isWeixin = reg.test(navigator.userAgent);
+  , ios = /iPhone OS/
+  , android = /android/i
+  , isWeixin = reg.test(navigator.userAgent)
+  , isIOS = ios.test(navigator.userAgent)
+  , isAndroid = android.test(navigator.userAgent);
 if (isWeixin) {
   document.body.addEventListener('click', function (event) {
     if (event.target.className === 'download-button' ||
@@ -10,4 +14,10 @@ if (isWeixin) {
       document.getElementById('cover').className = 'show';
     }
   }, false);
+}
+if (isIOS) {
+  document.body.className = 'ios';
+}
+if (isAndroid) {
+  document.body.className = 'android';
 }
