7bd78fa51845d7c0d30775259929f9ec0b3a6e9e
baibai <xiangbai89314@gmail.com>
2014-07-25 18:45:44 +0800

    添加微信页面下载失败提示

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 /**
  * Created by 路佳 on 2014/7/24.
  */
-var reg = /iPhone OS (\d+)/
-  , platform = navigator.userAgent.match(reg);
-if (platform && platform[1] === '7') {
-  var button = document.getElementsByClassName('download-button')[1];
-  button.innerHTML = '敬请期待';
-  button.classList.add('disabled');
-  button.href = 'javascript:void(0);';
+var reg = /micromessenger/i
+  , isWeixin = reg.test(navigator.userAgent);
+if (isWeixin) {
+  document.body.addEventListener('click', function (event) {
+    if (event.target.className === 'download-button' ||
+      event.target.parentNode.className === 'download-button') {
+      document.getElementById('cover').className = 'show';
+    }
+  }, false);
 }
